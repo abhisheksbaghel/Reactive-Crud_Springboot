0e8ef0fc5b76d210c2aa44d703aefa1679ae505c
abhisheksbaghel <abhisheksinghbaghel8@gmail.com>
2024-03-18 19:02:26 +0530

    integrated amqp

--- src/main/java/com/springboot/reactive/ReactiveApplication.java
+++ src/main/java/com/springboot/reactive/ReactiveApplication.java
@@ -1,11 +1,29 @@
 package com.springboot.reactive;
 
+import com.springboot.reactive.configs.DestinationsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DestinationsConfig.class)
 public class ReactiveApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter() {
+			@Override
+			public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+				return chain.filter(exchange);
+			}
+		};
 	}
 }
